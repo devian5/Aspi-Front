cff1fb188c4bca55aff29a294b3ceb2cc72c96df
devian5 <acgarage@outlook.es>
2021-05-01 18:07:27 +0200

    Created search component

--- app/src/components/search/Search.jsx
+++ app/src/components/search/Search.jsx
@@ -8,7 +8,6 @@ const Search = (props) => {
         <div>
             <nav class="navbar navbar-light bg-light">
                 <div class="container-fluid">
-                    <form class="d-flex">
                     <input 
                         class="form-control me-2" 
                         type={props.type} 
@@ -26,9 +25,29 @@ const Search = (props) => {
                         onClick={props.onClick}
                     >{props.children}</button>
 
-                    </form>
                 </div>    
             </nav>
+            {/* <div class="container-fluid">
+                
+                <input 
+                    class="form-control me-2" 
+                    type={props.type} 
+                    name={props.name} 
+                    value={props.value}
+                    onChange={props.onChange} 
+                    onKeyDown={props.onKeyDown} 
+                    aria-label="Search"
+
+                />
+
+                <button 
+                    class="btn btn-outline-success" 
+                    type="submit" 
+                    onClick={props.onClick}
+                >{props.children}</button> */}
+
+               
+                
         </div>
     )
 }
