92e38a2e86f9eb5ad46ce469d0ab351b778c0c64
devian5 <acgarage@outlook.es>
2021-05-02 02:29:30 +0200

    Modified search component with sass

--- app/src/components/search/Search.jsx
+++ app/src/components/search/Search.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
 const Search = (props) => {
-    
-    // console.log(props);
-    
+        
     return (
-        <div>
-            <nav class="navbar navbar-light bg-light">
+        <div className="searchNav">
+            {/* <nav class="navbar navbar-light bg-light"> */}
                 <div class="container-fluid">
                     <input 
                         class="form-control me-2" 
@@ -18,36 +16,20 @@ const Search = (props) => {
                         aria-label="Search"
 
                     />
+                    
+                    <div className="searchBtn">
+                        <button 
+                            class="btn btn-outline-success" 
+                            type="submit" 
+                            onClick={props.onClick}
+                        >
+                            {props.children}
+                        </button>
 
-                    <button 
-                        class="btn btn-outline-success" 
-                        type="submit" 
-                        onClick={props.onClick}
-                    >{props.children}</button>
+                    </div>
 
                 </div>    
-            </nav>
-            {/* <div class="container-fluid">
-                
-                <input 
-                    class="form-control me-2" 
-                    type={props.type} 
-                    name={props.name} 
-                    value={props.value}
-                    onChange={props.onChange} 
-                    onKeyDown={props.onKeyDown} 
-                    aria-label="Search"
-
-                />
-
-                <button 
-                    class="btn btn-outline-success" 
-                    type="submit" 
-                    onClick={props.onClick}
-                >{props.children}</button> */}
-
-               
-                
+            {/* </nav> */}
         </div>
     )
 }
