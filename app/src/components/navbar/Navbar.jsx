0851d285c3fd5cc5f3d961f9a2b64cc6b6fe3659
devian5 <acgarage@outlook.es>
2021-05-03 23:36:22 +0200

    Modified navbar component

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import logo from '../../img/logo.png';
 const Navbar = (props) => {
     return (
         <div className="nvb">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
             <div class="container-fluid">
                 <div >
                     <a className="logoProfile" href="/profile">
@@ -13,11 +13,12 @@ const Navbar = (props) => {
                     </a>
                     
                 </div>
+                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             {/* <a class="nav-link active" aria-current="page" href="#">Home</a> */}
                             <a class="nav-link" href="#">Emociones</a>
@@ -43,6 +44,7 @@ const Navbar = (props) => {
         </nav>
             
         </div>
+            
     )
 }
 
