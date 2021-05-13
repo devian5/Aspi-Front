ffed9778e08989980ed5997e4de9e55c20ff6832
devian5 <acgarage@outlook.es>
2021-05-13 13:53:59 +0200

    Modified card img

--- app/src/components/cardImg/CardImg.jsx
+++ app/src/components/cardImg/CardImg.jsx
@@ -4,19 +4,21 @@ const CardImg = (props) => {
 
     console.log(props);
     return (
-        <div>
-            <div class="card">
-              <img src={props.picture} class="card-img-top" alt="feeling"/>
-                <div class="card-body">
-                
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div className="cardImgFeeling">
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-12">
+                        <img src={props.picture} alt="ImgFeeling"/>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="card-body">
+                            <h5 class="card-title">{props.name}</h5>
+                            <p class="card-text">{props.description}</p>
+                            <p class="card-text">{props.example}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            {/* <div class="card" >
-                <img src={props.picture} class="card-img-top" alt="psdsrops"></img>
-
-            </div> */}
-            {/* <img src={props.picture} alt="proprpr"/> */}
         </div>
     )
 }
